9313615ee1f9ed624ab14129079d44095ab53c55
Kevin Escobar <Kevin@mongefranco.com>
2019-10-11 17:21:52 -0600

    perfil

--- js/LecturaArchivo.js
+++ js/LecturaArchivo.js
@@ -15,15 +15,24 @@ function onChange(event) {
 
 function onLoad() {
   var result = reader.result; // Obtienes el texto
-  // En tu ejemplo lo obtienes de una petición HTTP
-  
+  //Separa las lineas del archivo
   var lineas = result.split('\n');
   
   
   // o lineas.forEach(function(linea){ ... })
   // o lineas.find(function(linea){ return linea == contraseña })
+  var lineaActual = 0;
+
   for(var linea of lineas) {
+      lineaActual += 1;
+
     console.log('[linea]', linea)
+
+    var texto;
+    texto = document.getElementById("salidaTexto");
+
+    texto.value = texto.value + (lineaActual + linea);
+
     //if(linea === passwordBuscar) {
       // Encontraste contraseña
     //}
